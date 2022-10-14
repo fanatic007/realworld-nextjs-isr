22fdff56373f3b26104cfd25df13c0a236c01aaf
Sarang Tayde <sarang.tayde@gmail.com>
2022-10-14 07:43:51 +0530

    get profile bugfix (username->profileSlug)

--- pages/api/profiles/[username]/index.ts
+++ pages/api/profiles/[username]/index.ts
@@ -1,12 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { profileResponseFields } from '../../../../constants';
 import { getProfileWithFollowedBy } from '../../../../db/profile';
 import { getUser } from '../../../../db/user';
 import { apiHandler } from '../../../../helpers/api-handler';
 import { getJWTPayload } from '../../../../helpers/jwt-middleware';
 import { getResponse } from '../../../../helpers/type-helpers';
-import { ProfilePayload, ProfileResponse } from '../../../../types/index';
+import { ProfileResponse } from '../../../../types/index';
 
 export default apiHandler(handler);
 
@@ -20,7 +19,7 @@ async function handler (
       const token = (req.headers.authorization as string).replace('Bearer ','');
       const {username} = getJWTPayload(token);
       const user = await getUser({username}, {id:true});
-      const profileWithFollowedBy = await getProfileWithFollowedBy(username, user.id);
+      const profileWithFollowedBy = await getProfileWithFollowedBy(profileSlug, user.id);
       const profileResponse = getResponse<ProfileResponse>(profileWithFollowedBy,'profile') as ProfileResponse;
       return res.status(200).json(profileResponse);
     }

bfada7d91c8067edbfbd2ba3c4c5e79a4801f318
Sarang Tayde <sarang.tayde@gmail.com>
2022-11-11 22:49:10 +0530

    first unit test added

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
 import { Prisma, Tags, User, Article, Comment } from '@prisma/client';
 import { articleWithComments, LOGIN_REQUEST, PROFILE_PAYLOAD, TAGS_RESPONSE, USER_REQUEST, USER_PAYLOAD, ARTICLE_REQUEST, ARTICLE_RESPONSE } from '../constants';
-
+import * as jest from '@types/jest';
 
 type NonEmptyArray<T> = [T, ...T[]];
 type Errors = { body:NonEmptyArray<Error>}

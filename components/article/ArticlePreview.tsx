55324195c169092b88d465cc927e6a5e86f09655
Sarang Tayde <sarang.tayde@gmail.com>
2022-11-20 20:19:30 +0530

    tags added in preview

--- components/article/ArticlePreview.tsx
+++ components/article/ArticlePreview.tsx
@@ -1,14 +1,15 @@
 import { SingleArticle } from '../../types'
 import Image from 'next/image';
+import { API_SUFFIX_PROFILE } from '../../constants';
 
 const ArticlePreview = (props:any) => {
   const article = props.article as SingleArticle;
   return (
     <div className="article-preview">
       <div className="article-meta">
-          <a href="profile.html"><Image loader={ () => article.author.image} src="*.png" width={32} height={32}/></a>
+          <a href={API_SUFFIX_PROFILE}><Image loader={ () => article.author.image} src="*.png" width={32} height={32}/></a>
           <div className="info">
-              <a href="/profile" className="author">{article.author.username}</a>
+              <a href={API_SUFFIX_PROFILE} className="author">{article.author.username}</a>
               <span className="date">{(new Date(article.createdAt).toDateString())}</span>
           </div>
           <button className="btn btn-outline-primary btn-sm pull-xs-right">
@@ -16,16 +17,14 @@ const ArticlePreview = (props:any) => {
           </button>
       </div>
       <a href="" className="preview-link">
-          <h1>{article.title}</h1>
-          <p>{article.description}</p>
-          <span>Read more...</span>
-          {
-            article.tagList.map(tag=>
-                <ul className="tag-list">
-                    <li className="tag-default tag-pill tag-outline" key={`tag_${new Date()}`}>{tag}</li>
-                </ul>
-            )
-          }
+        <h1>{article.title}</h1>
+        <p>{article.description}</p>
+        <span>Read more...</span>          
+        <ul className="tag-list">
+            {
+                article.tagList.map(tag=> <li className="tag-default tag-pill tag-outline" key={`${tag}}`}>{tag}</li> )
+            }
+        </ul>
       </a>
       
     </div>
